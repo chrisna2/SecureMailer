5ee0a4a591ff7b68a0e35eaaaea3301b73ded487
chrisna2 <marksman703@gmail.com>
2023-04-26 16:10:07 +0900

    실습 1, 2 실행

--- src/com/securecoding/securemailer/App.java
+++ src/com/securecoding/securemailer/App.java
@@ -7,8 +7,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class App extends Application {
+	public static Logger logger = Logger.getLogger(App.class.getName());
     public static Stage stage;
     @Override
     public void start(Stage stage) {    
@@ -16,7 +19,12 @@ public class App extends Application {
         	Parent root = FXMLLoader.load(getClass().getResource("/resources/fxml/Main.fxml"));
             stage.setScene(new Scene(root));
         } catch(IOException ioe) {
-            ioe.printStackTrace();
+        	
+        	//패치방안 1 - 주석처리
+        	//ioe.printStackTrace();
+        	
+        	//패치방안 2 - Logging
+        	logger.log(Level.INFO, ioe.getMessage(), ioe);
         }
 
         this.stage = stage;
--- src/com/securecoding/securemailer/util/Tools.java
+++ src/com/securecoding/securemailer/util/Tools.java
@@ -3,13 +3,21 @@ package com.securecoding.securemailer.util;
 import java.io.File;
 import java.util.Scanner;
 
+import org.apache.commons.io.FilenameUtils;
+
 public class Tools {
 
     public static String loadTemplateFile(String filename) {
         try {
-            File file = new File(String.format("%s\\%s.txt", Constants.HTML_TEMPLATE_LOCATION, filename, ".txt"));
 
-            StringBuilder fileContents = new StringBuilder((int) file.length());
+        	//취약점 1 - scary 단계 -> 경로를 찾아가는 문자 열 제거
+        	//File file = new File(String.format("%s\\%s.txt", Constants.HTML_TEMPLATE_LOCATION, filename, ".txt"));
+
+        	//솔루션1 - spotBug - org.apache.commons.io.FilenameUtils 사용, 해당 라이브러리 build 처리
+        	String pathName = "C:\\SecureCoding_Expert\\workspace\\SecureMailer\\src\\resources\\template";
+        	File file = new File(pathName,FilenameUtils.getName(filename)+".txt");
+        	
+        	StringBuilder fileContents = new StringBuilder((int) file.length());
 
             try (Scanner scanner = new Scanner(file)) {
                 while (scanner.hasNextLine()) {
